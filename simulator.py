7d3865c0011544aaab3175a52dd358e0a9e35d05
Idris <iawardere3@gmail.com>
2024-02-24 11:17:33 -0500

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -67,15 +67,41 @@ def get_win_prob(N: int, num_games: int) -> None:
     print(f"Probability of winning: {win_prob}\n")
 
 
+def get_unique_wins(N: int, num_games: int):
+    games = simulate_games(N=N, num_games=num_games)
+
+    game_lists = set()
+    for game in games:
+        if game.won:
+            game_lists.add(tuple(game.game))
+
+    game_lists = list(game_lists)
+    game_strs = []
+    for tup in game_lists:
+        game_str = str(tup)
+        for repl in ["(", ")", " ", ","]:
+            game_str = game_str.replace(repl, "")
+        game_str = game_str.replace("1", "H")
+        game_str = game_str.replace("0", "T")
+        game_strs.append(game_str)
+
+    print(f"N={N}, num_games={num_games}, # winning combos={len(game_strs)}")    
+    for game_str in sorted(game_strs):
+        print(game_str)
+
 def get_expected_payout(N: int, num_games: int) -> None:
     games = simulate_games(N=N, num_games=num_games)
     total_payout = 0
+    # payouts = []
     for game in games:
         total_payout += game.payout
+        # payouts.append(game.payout)
     expected_payout = total_payout/len(games)
+    
 
     print(f"N={N}, num_games={num_games}")
     print(f"Expected payout: {expected_payout}\n")
+    # print(f"Payouts:  {payouts}\n")
 
 
 def main():
@@ -84,10 +110,24 @@ def main():
     # print("gg")
 
     # Problem a
-    # get_win_prob(6, 1000000)
+    # get_win_prob(6, 10000000)
+
+    # Problem b
+    # get_expected_payout(N=6, num_games=10000000)
+
+    # Problem c
+    get_win_prob(1000, 1000000)
+
+    # Problem d
+    # get_expected_payout(N=1000, num_games=10000000)
+
 
     # get_win_prob(N=8, num_games=1000000)
-    get_expected_payout(N=6, num_games=1000000)
+    # get_expected_payout(N=6, num_games=10000000)
+    # get_win_prob(N=1000, num_games=10000000)
+    # get_expected_payout(N=1000, num_games=1000000)
+
+    # get_unique_wins(N=10, num_games=1000000)
 
 
 if __name__ == "__main__":
